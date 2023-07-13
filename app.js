9e8005b4437710f211a587cb7a0f4876a288b06e
Kirill <117633274+04Fox04@users.noreply.github.com>
2023-07-13 13:38:59 +0300

    test 2

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const { celebrate, Joi, errors } = require('celebrate');
+const handleError = require('./middlewares/errors');
+const NotFoundError = require('./errors/NotFoundError');
+
+const app = express();
+mongoose.connect('mongodb://localhost:27017/mestodb', { family: 4 });
+const { createUser, login } = require('./controllers/users');
+const auth = require('./middlewares/auth');
+const usersRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
+
+app.use(express.json());
+app.use(helmet());
+app.use(cookieParser());
+
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(/^:?https?:\/\/(www\.)?[a-zA-Z\d-]+\.[\w\d\-.~:/?#[\]@!$&'()*+,;=]{2,}#?$/),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), createUser);
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), login);
+app.use(auth);
+app.use('/users', usersRouter);
+app.use('/cards', cardsRouter);
+app.use('*', (req, res, next) => {
+  next(new NotFoundError('Страница не найдена'));
+});
+
+app.use(errors());
+app.use(handleError);
+
+app.listen(3000, () => {
+  console.log('Сервер запущен!');
+});
